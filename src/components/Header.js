306fc01e9a827a5bc9b6f73cafa154560c7f34a5
sujinRo <shtnwls1111@ewhain.net>
2024-07-02 22:25:14 +0900

    feat: 쇼핑백에 갯수 표시

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { CartIcon } from '../assets/icons';
+import { useSelector } from 'react-redux';
 
 const Container = styled.div`
   width: 100%;
@@ -22,17 +23,39 @@ const Title = styled.div`
 `;
 
 const Icon = styled.div`
+  position: relative;
   width: 30px;
   cursor: pointer;
   color: white;
 `;
 
+const Circle = styled.div`
+  position: absolute;
+  left: 18px;
+  bottom: 18px;
+  width: 17px;
+  height: 17px;
+  text-align: center;
+  border-radius: 100%;
+  border: none;
+  color: white;
+  background-color: #8f89ff;
+  font-size: 11px;
+  font-weight: 600;
+`;
+
 export default function Header() {
+  const state = useSelector((state) => state.player);
+  let total = 0;
+
+  state.map((item) => (total += item.amount));
+
   return (
     <Container>
       <Title>UMC PlayList</Title>
       <Icon>
         <CartIcon />
+        <Circle>{total}</Circle>
       </Icon>
     </Container>
   );
